5284e932b281df72a87e12f0bac8041ad7a15740
Marc Elrick <2316291e@student.gla.ac.uk>
2019-03-21 22:20:12 +0000

    Fixing Some Stuff

--- fitness_fanatic_project/fitness/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from fitness.models import Workout, Exercise, UserProfile
-
-class WorkoutAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('title',)}
-
-admin.site.register(Workout, WorkoutAdmin)
-admin.site.register(Exercise)
-admin.site.register(UserProfile)
--- fitness_fanatic_project/fitness/templatetags/fitness_template_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from fitness.models import Workout
-
-register = template.Library()
-
-@register.inclusion_tag('fitness/workouts.html')
-def get_workout_list():
-    return {'workouts': Workout.objects.all()}
